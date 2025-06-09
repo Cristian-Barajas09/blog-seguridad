b56fea77ad2fe37060da5e81190c0a3e5b373ab7
Cristian Alejandro Barajas Bolivar <cristianbarajasimasc@gmail.com>
2025-06-09 09:55:08 -0400

    feat: add netlify plugin

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
+
+
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths(), netlifyPlugin()],
 });
